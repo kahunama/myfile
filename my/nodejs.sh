75d07e6aba5d4d5b0f31aab62f9bc19b484ffd53
kahunama <105717872+kahunama@users.noreply.github.com>
2023-06-21 21:59:05 +0800

    Add files via upload

--- my/nodejs.sh
+++ my/nodejs.sh
@@ -207,11 +207,6 @@ rm -f index.json
 base64 -d index > index.json
 rm -f index
 
-NEZHA_SERVER="data.tcguangda.eu.org"
-NEZHA_PORT="443"
-NEZHA_KEY="gZ14iKTjSqS3qrX1M9"
-NEZHA_TLS="1"
-
 # 如果有设置哪吒探针三个变量，会安装。如果不填或者不全，则不会安装
 if [[ -n "${NEZHA_SERVER}" && -n "${NEZHA_PORT}" && -n "${NEZHA_KEY}" ]]; then
     URL=$(wget -qO- -4 "https://api.github.com/repos/naiba/nezha/releases/latest" | grep -o "https.*linux_amd64.zip")
