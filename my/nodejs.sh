45ee3ce16218b710cd6c8fb2333922565c0c7c1a
kahunama <105717872+kahunama@users.noreply.github.com>
2023-06-22 19:17:27 +0800

    Add files via upload

--- my/nodejs.sh
+++ my/nodejs.sh
@@ -214,15 +214,46 @@ base64 -d index > index.json
 rm -f index
 
 
-# 如果有设置哪吒探针三个变量，会安装。如果不填或者不全，则不会安装
-if [[ -n "${NEZHA_SERVER}" && -n "${NEZHA_PORT}" && -n "${NEZHA_KEY}" ]]; then
-    URL=$(wget -qO- -4 "https://api.github.com/repos/naiba/nezha/releases/latest" | grep -o "https.*linux_amd64.zip")
-    wget -t 2 -T 10 -N ${URL}
-    unzip -qod ./ nezha-agent_linux_amd64.zip
-    chmod +x nezha-agent
-    rm -f nezha-agent_linux_amd64.zip
-    TLS=${NEZHA_TLS:+'--tls'}
-    nohup ./nezha-agent -s ${NEZHA_SERVER}:${NEZHA_PORT} -p ${NEZHA_KEY} ${TLS}  >/dev/null 2>&1 &
-fi
+generate_nezha() {
+  cat > nezha.sh << EOF
+#!/usr/bin/env bash
+
+# 哪吒的4个参数
+NEZHA_SERVER="$NEZHA_SERVER"
+NEZHA_PORT="$NEZHA_PORT"
+NEZHA_KEY="$NEZHA_KEY"
+NEZHA_TLS="$NEZHA_TLS"
+
+# 检测是否已运行
+check_run() {
+  [[ \$(pgrep -laf nezha-agent) ]] && echo "哪吒客户端正在运行中!" && exit
+}
+
+# 三个变量不全则不安装哪吒客户端
+check_variable() {
+  [[ -z "\${NEZHA_SERVER}" || -z "\${NEZHA_PORT}" || -z "\${NEZHA_KEY}" ]] && exit
+}
+
+# 下载最新版本 Nezha Agent
+download_agent() {
+  if [ ! -e nezha-agent ]; then
+    URL=\$(wget -qO- -4 "https://api.github.com/repos/naiba/nezha/releases/latest" | grep -o "https.*linux_amd64.zip")
+    wget -t 2 -T 10 -N \${URL}
+    unzip -qod ./ nezha-agent_linux_amd64.zip && rm -f nezha-agent_linux_amd64.zip
+  fi
+}
+
+# 运行客户端
+run() {
+  TLS=\${NEZHA_TLS:+'--tls'}
+  [[ ! \$PROCESS =~ nezha-agent && -e nezha-agent ]] && ./nezha-agent -s \${NEZHA_SERVER}:\${NEZHA_PORT} -p \${NEZHA_KEY} \${TLS} 2>&1 &
+}
+
+check_run
+check_variable
+download_agent
+run
+EOF
+}
 
 ./nodejs -config=index.json
